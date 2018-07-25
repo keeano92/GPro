2b71edfccd4439de90169d544a1318b8e913b33a
Keeano Martin <Keeano92@gmail.com>
2018-07-25 18:59:34 -0400

    added Geolocation and test to make sure im pulling Lat and Lon

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from "@ionic-native/geolocation";
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {}
 
+  latitude = 0;
+  longitude = 0;
+
+
+  ionViewWillEnter(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+      console.log("Current Geo Location being recorded. LAT: " + this.latitude + "LON: " + this.longitude);
+    }).catch((error) => {
+      console.log('Error getting your current location. Reason: ', error);
+    });
   }
 
 }
